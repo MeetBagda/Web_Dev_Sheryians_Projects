f3c275c71f8a0549a5c5494c886cce5c98d6610e
MeetBagda <bagdameet@gmail.com>
2023-12-14 22:22:19 +0530

    api_consuming_react

--- react_app_nextjs_sheryians/app/page.js
+++ react_app_nextjs_sheryians/app/page.js
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client"
+import Header from '@/Components/Header';
+import React, { useState } from 'react'
 
 const page = () => {
+  let [count, setCount] = useState(1)
+  const [user, setUser] = useState("MEET")
+  function increment(){
+    setCount(count + 1);
+  }
   return (
-    <div>page</div>
+    <>
+      <h1 className='text-3xl text-red-500 font-bold bg-blue-300'>Count : {count}</h1>
+      <button className='bg-slate-400 rounded mt-5' onClick={()=>{increment()}}>Increment</button>
+      <Header user={user}/>
+    </>
   )
 }
 
