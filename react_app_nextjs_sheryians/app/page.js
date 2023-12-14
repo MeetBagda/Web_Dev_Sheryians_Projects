63fab16e9ec74f792065a03b422d4e8722cc4966
MeetBagda <bagdameet@gmail.com>
2023-12-14 22:22:47 +0530

    commit

--- react_app_nextjs_sheryians/app/page.js
+++ react_app_nextjs_sheryians/app/page.js
@@ -1,18 +1,31 @@
 "use client"
-import Header from '@/Components/Header';
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
-  let [count, setCount] = useState(1)
-  const [user, setUser] = useState("MEET")
-  function increment(){
-    setCount(count + 1);
+  const [images, setImages] = useState([]);
+  useEffect(()=>{
+    getImages()
+  },[])
+  const getImages = async ()=>{
+    try {
+      const response = await axios.get("https://picsum.photos/v2/list")
+      const data = response.data;
+      setImages(data) 
+    } catch (error) {
+      console.log("An error occurs while fetching images!!!")
+    }
   }
   return (
     <>
-      <h1 className='text-3xl text-red-500 font-bold bg-blue-300'>Count : {count}</h1>
-      <button className='bg-slate-400 rounded mt-5' onClick={()=>{increment()}}>Increment</button>
-      <Header user={user}/>
+      <button onClick={getImages} className='bg-green-400 rounded font-bold text-white px-5 py-5 mx-10 my-10 '>
+        Get Image
+      </button>
+      <div className='p-10'>
+        {images.map((elem,i)=>{
+          return <img key={i} src={elem.download_url} width={300} height={300} className='m-20 rounded inline-block'/>
+        })}
+      </div>
     </>
   )
 }
